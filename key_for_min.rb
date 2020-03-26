33a2afae584a65651fdb9c62d180f3115313d222
Diane Crame <dcrame@icloud.com>
2020-03-26 22:33:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  #iterate over the hash and return key that points to smallest value
+  #if passed empty hash, return nil
 end
